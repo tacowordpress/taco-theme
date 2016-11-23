fd73c7a5dc1c7879b4be5bd2296c05394701f16f
Ian Zhang <ianzhang@vermilion.com>
2016-11-23 10:34:57 -0700

    Adding hash to versioning

--- src/app/config.php
+++ src/app/config.php
@@ -1,7 +1,7 @@
 <?php
 
-// Cache busting
-// This automatically pulls from main.scss
-define('THEME_VERSION', (ENVIRONMENT !== ENVIRONMENT_PROD) ? mt_rand() : (int) preg_replace('/((?:.*)\$version\:\s+(\d+);(?:.*))/si', '$2', file_get_contents(__DIR__.'/_/scss/main.scss')));
-define('THEME_SUFFIX', sprintf('?v=%s', THEME_VERSION));
+// This automatically pulls from webpack_hash.  This will bust the cache whenever
+// the any js or css asset changes
+define('THEME_VERSION', file_get_contents(__DIR__.'/../_/dist/webpack_hash'));
+define('THEME_SUFFIX', sprintf('?ver=%s', THEME_VERSION));
 define('USER_SUPER_ADMIN', 'vermilion_admin'); // vermilion_admin
